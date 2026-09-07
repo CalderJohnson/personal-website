acdd01da3552ec80756114220fc50728b20d2952
Calder Johnson <calderbjohnson@icloud.com>
2026-09-07 18:29:28 -0400

    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Tells Next.js to generate static HTML
-  basePath: '/personal-website', // Replaces leading '/' with your repository path
   images: {
     unoptimized: true, // Disables server-side image optimization
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;

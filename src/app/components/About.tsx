0b08c61fa81d8192c761b6b8176aa31b4978c482
Calder Johnson <calderbjohnson@icloud.com>
2026-09-07 19:01:28 -0400

    Updated about me

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -10,7 +10,9 @@ const About = () => {
           <span className="eyebrow">AI Researcher and Software Engineer</span>
           <h1 className="page-title">Hi, I&apos;m Calder.</h1>
           <p className="intro-copy">
-            Modern artificial intelligence continues to rapidly transform the way we solve problems across every industry, giving us powerful operational automation and accelerated scientific discoveries. I work to develop robust, sophisticated machine learning models supporting data driven workflows and decision making, as well as software infrastructure to optimally apply AI to problems in industry, emphasizing safe and performant integration. <br></br><br></br>Currently, I am pursuing my Masters in Computer Science at the University of Waterloo where my research focuses on model explanation methods as a foundation for monitoring efficacy and alignment, supporting powerful and trustworthy AI moving forward.
+            Modern artificial intelligence continues to rapidly transform the way we solve problems across every industry, giving us powerful operational automation and accelerated scientific discoveries. I work to develop robust, sophisticated machine learning models supporting data driven workflows and decision making, as well as software infrastructure to optimally apply AI to problems in industry, emphasizing safe and performant integration. 
+            <br></br><br></br>Currently, I am pursuing my Masters in Computer Science at the University of Waterloo where my research focuses on model explanation methods as a foundation for monitoring efficacy and alignment, supporting powerful and trustworthy AI moving forward.
+            <br></br><br></br>In my free time I enjoy playing perfect information games like Chess and Go, lifting weights, folding origami, and reading classic and science fiction as well as non-fiction on topics like philosophy, physics, biology and pure mathematics.
           </p>
         </div>
         <div>

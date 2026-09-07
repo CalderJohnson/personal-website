fb69c68ca19abed84e3889e8ebe290e428d15d3d
Calder Johnson <calderbjohnson@icloud.com>
2026-09-07 18:33:07 -0400

    Fixed build errors

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className="intro-grid">
         <div>
           <span className="eyebrow">AI Researcher and Software Engineer</span>
-          <h1 className="page-title">Hi, I'm Calder.</h1>
+          <h1 className="page-title">Hi, I&apos;m Calder.</h1>
           <p className="intro-copy">
             Modern artificial intelligence continues to rapidly transform the way we solve problems across every industry, giving us powerful operational automation and accelerated scientific discoveries. I work to develop robust, sophisticated machine learning models supporting data driven workflows and decision making, as well as software infrastructure to optimally apply AI to problems in industry, emphasizing safe and performant integration. <br></br><br></br>Currently, I am pursuing my Masters in Computer Science at the University of Waterloo where my research focuses on model explanation methods as a foundation for monitoring efficacy and alignment, supporting powerful and trustworthy AI moving forward.
           </p>
